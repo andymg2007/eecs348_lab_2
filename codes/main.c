5d971526ce2ef875e08909865a5bf0e55904771d
Garcia <a455g434@engr1005c-10.home.ku.edu>
2026-09-09 09:34:16 -0500

    Changed main.c for isOdd function

--- codes/main.c
+++ codes/main.c
@@ -1,4 +1,4 @@
-#include "isEven.h"
+#include "isOdd.h"
 
 #include <stdio.h>
 
@@ -6,6 +6,6 @@ int main() {
     int num;
     printf("Enter an integer: ");
     scanf("%d", &num);
-    isEven(num);
+    isOdd(num);
     return 0;
 }
